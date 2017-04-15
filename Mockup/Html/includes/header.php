98b27a941f8bd61007f5dfc7aee23ce53f0f3b69
EpicKiwi <me@epickiwi.fr>
2017-04-15 17:52:16 +0200

    Refonte du header

--- Mockup/Html/includes/header.php
+++ Mockup/Html/includes/header.php
@@ -12,28 +12,30 @@
     <?php if(isset($customHead)){ echo $customHead; }?>
 </head>
 <body>
-<header class="container-fluid header">
-    <a href="#" class="logo"><img src="/static/img/logobde.svg" alt="logo" /></a>
-    <nav class="navbar row">
-        <div class="col-md-9 nav">
-            <div class="row">
-                <a href="#" class="col-md-2"><i class="fa fa-home" aria-hidden="true"></i>Home</a>
-                <a href="#" class="col-md-2"><i class="fa fa-calendar" aria-hidden="true"></i>Events</a>
-                <a href="#" class="col-md-2"><i class="fa fa-picture-o" aria-hidden="true"></i>Gallery</a>
-                <a href="#" class="col-md-2"><i class="fa fa-shopping-cart" aria-hidden="true"></i>Shop</a>
-                <a href="#" class="col-md-3"><i class="fa fa-users" aria-hidden="true"></i>Members</a>
+<header class="container-fluid main-header-container">
+    <div class="main-header row no-gutters">
+        <a href="/" class="main-header-logo col-12 col-md-1">
+            <img class="main-header-logo-img" src="/static/img/logobde.svg" alt="logo" />
+        </a>
+        <nav class="main-navbar-container col-12 col-md-8">
+            <div class="row justify-content-left main-navbar">
+                <a href="/" class="col-12 col-md-auto"><i class="fa fa-home" aria-hidden="true"></i>Home</a>
+                <a href="/events.php" class="col-12 col-md-auto"><i class="fa fa-calendar" aria-hidden="true"></i>Events</a>
+                <a href="#" class="col-12 col-md-auto"><i class="fa fa-picture-o" aria-hidden="true"></i>Gallery</a>
+                <a href="#" class="col-12 col-md-auto"><i class="fa fa-shopping-cart" aria-hidden="true"></i>Shop</a>
+                <a href="#" class="col-12 col-md-auto"><i class="fa fa-users" aria-hidden="true"></i>Members</a>
+            </div>
+        </nav>
+        <div class="col-12 col-md-3 main-header-connection-container">
+            <div class="row justify-content-center main-header-connection align-items-center">
+                <div class="col-md-5 main-header-connection-btn-container">
+                    <a href="#" class="main-header-connection-btn" >Log in</a>
+                </div>
+                <div class="col-md-5 main-header-connection-btn-container">
+                    <a href="#" class="main-header-connection-btn">Sign in</a>
+                </div>
             </div>
         </div>
-        <div class="col-md-3 connection">
-            <div class="row">
-                <div class="col-md-5 login">
-                    <a href="#" >Log in</a>
-                </div>
-                <div class="col-md-5 singin">
-                    <a href="#">Sign in</a>
-                </div>
-            </div>
-        </div>
-    </nav>
+    </div>
 </header>
 <section class="container main-page">
