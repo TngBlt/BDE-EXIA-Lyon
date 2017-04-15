27177bdb5aad120255b5b107fecf2f460250cd16
EpicKiwi <me@epickiwi.fr>
2017-04-15 17:59:14 +0200

    Refonte du footer

--- Mockup/Html/includes/footer.php
+++ Mockup/Html/includes/footer.php
@@ -2,16 +2,15 @@
 <footer class="container-fluid footer">
     <div class="container">
         <div class="row">
-            <div class="col-md-4 footer-infos">
+            <div class="col-12 col-md-4 footer-infos">
                 <ul>
-                    <li><h4 class="title"><a href="#">Legals Notice</a></h4></li>
-                    <li><h4 class="title"><a href="#">About</a></h4></li>
-                    <li><h4 class="title"><a href="#">Contact</a></h4></li>
-                    <li><h4 class="title"><a href="#">FAQ</a></h4></li>
+                    <li><a href="#">Legals Notice</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                    <li><a href="#">FAQ</a></li>
                 </ul>
             </div>
-            <diV class="col-md-4 footer-social">
-                <h2 class="title">Social</h2>
+            <diV class="col-12 col-md-4 footer-social">
                 <div class="footer-social-logos">
                     <a href="#" target="_blank"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
                     <a href="#" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
@@ -19,10 +18,10 @@
                     <a href="#" target="_blank"><i class="fa fa-snapchat" aria-hidden="true"></i></a>
                 </div>
             </div>
-            <div class="col-md-4 footer-logos">
+            <div class="col-12 col-md-4 footer-logos">
                 <div class="row">
-                    <div class="col-md-6"><img src="/static/img/exia.png" alt="eXia" width=85%/></div>
-                    <div class="col-md-6"><img src="/static/img/ei.png" alt="ei" width=90%/></div>
+                    <div class="col-6"><img src="/static/img/exia.png" alt="eXia" width=85%/></div>
+                    <div class="col-6"><img src="/static/img/ei.png" alt="ei" width=90%/></div>
                 </div>
             </div>
         </div>
