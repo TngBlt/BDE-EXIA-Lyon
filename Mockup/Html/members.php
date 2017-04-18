740344a406d0029dea6080c3dccb325cb2675481
Tanguy <tanguy.blochet@viacesi.fr>
2017-04-18 09:12:12 +0000

    Members updated

--- Mockup/Html/members.php
+++ Mockup/Html/members.php
@@ -69,7 +69,7 @@ include "includes/header.php"; ?>
 
     ?>
     <div class="content-title">
-        <h2>Student Union Team</h2>
+        <h2 class="section-title">Student Union Team</h2>
     </div>
 
     <div class="row members">
@@ -103,38 +103,35 @@ include "includes/header.php"; ?>
 
 
     <div class="content-title other-members-title">
-        <h2>Other members</h2>
+        <h2 class="section-title">Other members</h2>
     </div>
 
-    <div class="row members">
+    <div class="row other-members">
         <?php
         $otherMembersJSON = json_decode($otherMembers);
 
-        foreach ($otherMembersJSON->members as $member):?>
-            <a href="<?php echo $member->name; ?>">
+        foreach ($otherMembersJSON->members as $otherMember):?>
+            <a href="<?php echo $otherMember->name; ?>">
                 <div class="col-lg-4 widgets">
                     <div class="widget widget-primary other-member">
-                        <?php if (isset($member->avatar)): ?>
+                        <?php if (isset($otherMember->avatar)): ?>
                             <div class="member-avatar">
-                                <img src="/static/img/members/<?php echo $member->avatar; ?>" alt="member-avatar" width="65%">
+                                <img src="/static/img/members/<?php echo $otherMember->avatar; ?>" alt="member-avatar" width="65%">
                             </div>
                         <?php endif; ?>
                         <div class="member-info">
-                            <p class="member-name"><?php echo $member->name; ?></p>
-                            <p class="member-class"><?php echo $member->prom; ?></p>
+                            <p class="member-name"><?php echo $otherMember->name; ?></p>
+                            <p class="member-class"><?php echo $otherMember->prom; ?></p>
                         </div>
                         <div class="member-icons">
-                            <a href="mailto:<?php echo $member->email; ?>"><i class="fa fa-envelope" aria-hidden="true"></i></a>
-                            <a href="<?php echo $member->facebook; ?>" target="_blank"><i class="fa fa-facebook-official" aria-hidden="true"></i></a>
+                            <a href="mailto:<?php echo $otherMember->email; ?>"><i class="fa fa-envelope" aria-hidden="true"></i></a>
+                            <a href="<?php echo $otherMember->facebook; ?>" target="_blank"><i class="fa fa-facebook-official" aria-hidden="true"></i></a>
                             <a href="#"><i class="fa fa-user" aria-hidden="true"></i></a>
                         </div>
                     </div>
                 </div>
             </a>
         <?php endforeach; ?>
-    </div>
-
-    <div class="content-title">
     </div>
 
 </div>
