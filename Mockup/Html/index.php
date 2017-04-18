492cbab5e31ed5913d7bc1033e4085570838ce47
Tanguy <tanguy.blochet@viacesi.fr>
2017-04-18 17:49:09 +0000

    Mockup ended

--- Mockup/Html/index.php
+++ Mockup/Html/index.php
@@ -7,7 +7,7 @@ $customHead =  "<link rel=\"stylesheet\" type=\"text/css\" href=\"/static/css/ar
 include "includes/header.php"; ?>
 
 <div class="row">
-	<section class="col-12 col-md-8">
+    <section class="col-12 col-md-8">
         <h2 class="section-title">Events to come</h2>
 
         <article class="event">
@@ -37,7 +37,8 @@ include "includes/header.php"; ?>
             <div class="container-fluid">
                 <nav class="event-footer row">
                     <a href="#" class="event-footer-item event-commentbtn col-4">
-                        <div class="event-footer-item-icon"><i class="fa fa-comment-o" aria-hidden="true"></i></i></div>
+                        <div class="event-footer-item-icon">
+                            <i class="fa fa-comment-o" aria-hidden="true"></i></div>
                         <div class="event-footer-item-title">Comment</div>
                     </a>
                     <a href="#" class="event-footer-item event-participatebtn col-4">
@@ -50,9 +51,9 @@ include "includes/header.php"; ?>
                     </a>
                 </nav>
             </div>
-        </article> 
+        </article>
 
-            <article class="event">
+        <article class="event">
             <div class="event-background-container">
                 <div class="event-background">
                     <img src="/static/img/stone.jpg" alt=""/>
@@ -60,7 +61,7 @@ include "includes/header.php"; ?>
                 <div class="container-fluid">
                     <div class="event-header row">
                         <h3 class="event-header-title col-11">
-                            <a href="#">
+                            <a href="/event.php">
                                 <span class="event-header-icon">
                                     <i class="fa fa-calendar" aria-hidden="true"></i>
                                 </span>
@@ -80,7 +81,7 @@ include "includes/header.php"; ?>
                 <nav class="event-footer row">
                     <a href="#" class="event-footer-item event-commentbtn col-4">
                         <div class="event-footer-item-icon"><i class="fa fa-comment-o" aria-hidden="true"></i>
-						</div>
+                        </div>
                         <div class="event-footer-item-title">Comment</div>
                     </a>
                     <a href="#" class="event-footer-item event-participatebtn col-4">
@@ -95,9 +96,9 @@ include "includes/header.php"; ?>
             </div>
         </article>
 
-		<div class="widget widget-primary">
-			<a href="events.php"  class="moreevents"><h5>More events</h5></a>
-		</div>
+        <div class="widget widget-primary">
+            <a href="events.php"  class="moreevents"><h5>More events</h5></a>
+        </div>
         <h2 class="section-title">Discover</h2>
 
         <article class="event">
@@ -117,7 +118,7 @@ include "includes/header.php"; ?>
                         <a href="#" class="event-header-sharebtn col-1"><i class="fa fa-share" aria-hidden="true"></i></a>
                     </div>
                 </div>
-            <div class="container-fluid">
+                <div class="container-fluid">
                     <div class="event-content row align-items-center justify-content-center">
                         <h4 class="event-content-main col-12">Monday 12 march</h4>
                         <h5 class="event-content-offset col-12">1 months ago</h5>
@@ -141,53 +142,58 @@ include "includes/header.php"; ?>
     </section>
 
     <section class="col-12 col-md-4">
-    	<aside class="vote-content">
-    	<div class="widget vote-widget">
-            <h4 class="widget-title widget-offset">Vote !</h4>
-            <p class="widget-offset vote-widget-text">Vote for the next event you want to see in this list !</p>
-            <div class="row justify-content-center widget-offset">
-                <a href="/vote.php" class="button button-primary col-auto">Vote now</a>
-            </div>
-        </div>
-    </aside>
-
-    <aside class="shop-content">
-    	<h4>Shop</h4>
-	<div class="row">
-		<div class="col-12 col-md-6 widget">
-                <div class="product-image">
-                    <img src="static/img/gallery/15.jpg" alt="FirstProduct" class="product1" />
+        <aside class="vote-content">
+            <div class="widget vote-widget">
+                <h4 class="widget-title widget-offset">Vote !</h4>
+                <p class="widget-offset vote-widget-text">Vote for the next event you want to see in this list !</p>
+                <div class="row justify-content-center widget-offset">
+                    <a href="/vote.php" class="button button-primary col-auto">Vote now</a>
                 </div>
-                <div class="product-desc">
-                    <h4 class="product-title">
-                        Item 1
-                    </h4>
-                    <div class="product-price">
-                        10€
-                        <nav class="product-menu">
-                            <a href="#"><i class="fa fa-cart-plus"></i></a>
-                        </nav>
+            </div>
+        </aside>
+
+        <aside class="shop-content">
+            <div class="widget">
+                <h4>Shop</h4>
+                <div class="row">
+                    <div class="col-12 col-md-6 product">
+                        <div class="product-image">
+                            <img src="static/img/gallery/15.jpg" alt="FirstProduct" class="product1" />
+                        </div>
+                        <div class="product-desc">
+                            <h4 class="product-title">
+                                Item 1
+                            </h4>
+                            <div class="product-price">
+                                10€
+                                <nav class="product-menu">
+                                    <a href="#"><i class="fa fa-cart-plus"></i></a>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6 product">
+                        <div class="product-image">
+                            <img src="static/img/gallery/4.jpg" alt="SecondProduct" class="product2" />
+                        </div>
+                        <div class="product-desc">
+                            <h4 class="product-title">
+                                Item 2
+                            </h4>
+                            <div class="product-price">
+                                15€
+                                <nav class="product-menu">
+                                    <a href="#"><i class="fa fa-cart-plus"></i></a>
+                                </nav>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
+        </aside>
+    </section>
+</div>
 
-            <div class="col-12 col-md-6 widget">
-                <div class="product-image">
-                    <img src="static/img/gallery/4.jpg" alt="SecondProduct" class="product2" />
-                </div>
-                <div class="product-desc">
-                    <h4 class="product-title">
-                        Item 2
-                    </h4>
-                    <div class="product-price">
-                        15€
-                        <nav class="product-menu">
-                            <a href="#"><i class="fa fa-cart-plus"></i></a>
-                        </nav>
-                    </div>
-                </div>
-            </div>
-    </aside>
-</section>           
+
 
 <?php include "includes/footer.php"; ?>
