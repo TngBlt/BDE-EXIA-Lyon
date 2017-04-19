0fbea451ec0456cf578a1bf14254e9318a14dac7
EpicKiwi <me@epickiwi.fr>
2017-04-19 09:48:15 +0200

    Base affichage members

--- src/src/UserBundle/Controller/DefaultController.php
+++ src/src/UserBundle/Controller/DefaultController.php
@@ -7,4 +7,22 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class DefaultController extends Controller
 {
+
+    /**
+     * @Route("/members",name="all_members")
+     */
+    public function membersAction(){
+
+        $membersRepo = $this->getDoctrine()->getRepository("UserBundle:User");
+
+        $members = $membersRepo->createQueryBuilder("user")
+            ->orderBy("user.username","DESC")
+            ->getQuery()->getResult();
+
+        return $this->render("UserBundle:Default:members.html.twig",[
+            "members"=>$members
+        ]);
+
+    }
+
 }
