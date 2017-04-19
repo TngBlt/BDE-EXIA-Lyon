e1d1ca52b2a5b6f79fd40ee060d96e392a55c954
EpicKiwi <me@epickiwi.fr>
2017-04-19 14:44:53 +0200

    Crud event

---
+++ src/src/EventBundle/Form/ActivityEventType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace EventBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ActivityEventType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('date')->add('title')->add('description')->add('price')->add('frequency')->add('initialProposition');
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'EventBundle\Entity\ActivityEvent'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'eventbundle_activityevent';
+    }
+
+
+}
