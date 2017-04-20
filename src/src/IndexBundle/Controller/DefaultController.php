00b71e944d910028c0e420c8004c3976e51de838
EpicKiwi <me@epickiwi.fr>
2017-04-20 13:23:01 +0200

    Page d'accueil

--- src/src/IndexBundle/Controller/DefaultController.php
+++ src/src/IndexBundle/Controller/DefaultController.php
@@ -12,6 +12,33 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('IndexBundle:Default:index.html.twig');
+        $em = $this->getDoctrine()->getManager();
+        $evRepo = $em->getRepository("EventBundle:ActivityEvent");
+        $prodRepo = $em->getRepository("ShopBundle:Product");
+
+        $nextEvents = $evRepo->createQueryBuilder("event")
+            ->where("event.date > CURRENT_DATE()")
+            ->orderBy("event.date")
+            ->setMaxResults(2)
+            ->getQuery()->getResult();
+
+        $pastEvents = $evRepo->createQueryBuilder("event")
+            ->where("event.date < CURRENT_DATE()")
+            ->orderBy("event.date", "DESC")
+            ->setMaxResults(2)
+            ->getQuery()->getResult();
+
+        $products = $prodRepo->findByAvailable(true);
+        $randProducts = [];
+
+        for($i = 0; $i<2; $i++){
+            array_push($randProducts,$products[random_int(0,count($products)-1)]);
+        }
+
+        return $this->render('IndexBundle:Default:index.html.twig',[
+            "nextEvents"=>$nextEvents,
+            "pastEvents"=>$pastEvents,
+            "products"=>$randProducts
+        ]);
     }
 }
